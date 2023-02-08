00251efc84c1087a7dca43ccd0f534a2dae7c285
iammmila <ibramila2002@gmail.com>
2023-02-08 01:18:17 -0800

    slider can't be modified fully

--- frontend/src/components/Main/Home/Slider/Slider.jsx
+++ frontend/src/components/Main/Home/Slider/Slider.jsx
@@ -14,19 +14,54 @@ import film4 from "../../../../assets/films_images/megan_saytt_resized.jpg"
 import film5 from "../../../../assets/films_images/my_shitdifejuheolndf_resized.jpg"
 
 // import required modules
-import { Navigation, FreeMode, Pagination } from "swiper";
+import { FreeMode, Navigation, Pagination } from "swiper";
 function Slider() {
 
   return (
     <section className='hero_section'>
       <div className="hero">
-        <div className="bg"></div>
-        <Swiper
-          // onSwiper={setSwiperRef}
-          slidesPerView={4}
+        {/* //! BACKGROUND SLIDER */}
+        
+        {/* <Swiper
+          spaceBetween={30}
+          slidesPerView={1}
           centeredSlides={true}
+          autoplay={{
+            delay: 4000,
+            disableOnInteraction: false,
+          }}
+          // pagination={{
+          //   clickable: true,
+          // }}
+          // navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="bg bcg-slider"
+        >
+          <SwiperSlide
+            style={{ backgroundImage: `url(${film1} )` }} className="bg bcg-slide">
+          </SwiperSlide>
+          <SwiperSlide
+            style={{ backgroundImage: `url(${film2} )` }} className="bg bcg-slide">
+          </SwiperSlide>
+          <SwiperSlide
+            style={{ backgroundImage: `url(${film3} )` }} className="bg bcg-slide">
+          </SwiperSlide>
+          <SwiperSlide
+            style={{ backgroundImage: `url(${film4} )` }} className="bg bcg-slide">
+          </SwiperSlide>
+          <SwiperSlide
+            style={{ backgroundImage: `url(${film5} )` }} className="bg bcg-slide">
+          </SwiperSlide>
+        </Swiper> */}
+
+
+<div className="bg"></div>
+        <Swiper
+          slidesPerView={4}
+          centeredSlides={true} 
           freeMode={true}
           // spaceBetween={10}
+          // autoplay={false}
           pagination={{
             clickable: true,
           }}
@@ -34,26 +69,26 @@ function Slider() {
           modules={[Pagination, FreeMode, Navigation]}
           className="mySwiper"
         >
-        <SwiperSlide>
-          <img className='films' width={"400px"} height={"600px"} src={film1} alt="film1" />
-        </SwiperSlide>
           <SwiperSlide>
-            <img className='films' width={"400px"} height={"600px"}  src={film2} alt="film1" />
+            <img className='films' width={"400px"} height={"600px"} src={film1} alt="film1" />
           </SwiperSlide>
           <SwiperSlide>
-            <img className='films' width={"400px"} height={"600px"}  src={film3} alt="film1" />
+            <img className='films' width={"400px"} height={"600px"} src={film2} alt="film1" />
           </SwiperSlide>
           <SwiperSlide>
-            <img className='films' width={"400px"}  height={"600px"} src={film4} alt="film1" />
+            <img className='films' width={"400px"} height={"600px"} src={film3} alt="film1" />
           </SwiperSlide>
           <SwiperSlide>
-            <img className='films' width={"400px"}  height={"600px"} src={film5} alt="film1" />
+            <img className='films' width={"400px"} height={"600px"} src={film4} alt="film1" />
           </SwiperSlide>
           <SwiperSlide>
-            <img  className='films' width={"400px"}  height={"600px"} src={film1} alt="film1" />
+            <img className='films' width={"400px"} height={"600px"} src={film5} alt="film1" />
           </SwiperSlide>
           <SwiperSlide>
-            <img className='films' width={"400px"}  height={"600px"} src={film2} alt="film1" />
+            <img className='films' width={"400px"} height={"600px"} src={film1} alt="film1" />
+          </SwiperSlide>
+          <SwiperSlide>
+            <img className='films' width={"400px"} height={"600px"} src={film2} alt="film1" />
           </SwiperSlide>
         </Swiper>
       </div>
