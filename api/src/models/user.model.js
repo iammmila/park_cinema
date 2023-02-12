21639edb66070e95b0850a11642dbd355222a54f
iammmila <ibramila2002@gmail.com>
2023-02-12 00:58:12 -0800

    localhost:5000 is connected

---
+++ api/src/models/user.model.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose")
+
+const userShema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    lastname: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    reset: {
+       code: {
+        type: String,
+        default: null
+       } ,
+       time: {
+        type: String,
+        default: null
+       }
+    }
+},{collection: "users", timestamps: true})
+const user = mongoose.model("users", userShema)
+
+module.exports = user
