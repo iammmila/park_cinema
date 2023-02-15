9a4ba956cb5aa5ef5bada574c9c60e1f56b683bd
iammmila <ibramila2002@gmail.com>
2023-02-15 11:15:05 -0800

    new branch is created and tested

--- frontend/src/components/Main/Technologies/LaseR/HeroSection/HeroSection.jsx
+++ frontend/src/components/Main/Technologies/LaseR/HeroSection/HeroSection.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import "./style.scss"
+import InformationImax from './../../IMAX/InformationImax/InformationImax';
 
 function HeroSection() {
     return (
-        <div>HeroSection</div>
+        <section className='imax-hero'>
+            <div className='bcgImage'>
+                <div className="container">
+                    <div className='wrapper-blur-section'>
+                        <div className='content'>
+                            <div className="blur-bg"></div>
+                            <div style={{ display: "flex", }}>
+                                <InformationImax />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
