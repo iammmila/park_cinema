10c27c52c686d091e7370354b67716d08933ee6d
iammmila <ibramila2002@gmail.com>
2023-02-16 08:56:09 -0800

    scroll top is fixed with context.

--- frontend/src/components/Main/ScrollTop/ScrollTopButton.jsx
+++ frontend/src/components/Main/ScrollTop/ScrollTopButton.jsx
@@ -1,26 +1,10 @@
-import React, { useEffect, useState } from 'react'
+import React, { useContext } from 'react'
 import "./style.scss"
 import { IoIosArrowUp } from "react-icons/io"
+import { MainContext } from '../../../context/ContextProvider';
 
 function ScrollTopButton() {
-    const [showTopBtn, setShowTopBtn] = useState(false);
-
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            if (window.scrollY > 400) {
-                setShowTopBtn(true);
-            } else {
-                setShowTopBtn(false);
-            }
-        });
-    }, []);
-
-    const goToTop = () => {
-        window.scrollTo({
-            top: 0,
-            behavior: "smooth",
-        });
-    };
+    const { showTopBtn, goToTop } = useContext(MainContext)
 
     return (
         <div className="top-to-btm">
--- frontend/src/context/ContextProvider.jsx
+++ frontend/src/context/ContextProvider.jsx
@@ -5,14 +5,30 @@ export const MainContext = createContext(null)
 function ContextProvider({ children }) {
     const [mode, setMode] = useState("dark");
     const [isOpen, setIsOpen] = useState(false);
+    const [showTopBtn, setShowTopBtn] = useState(false);
     const [isActive, setIsActive] = useState({
         id: 'today',
     })
-    
+
     useEffect(() => {
         console.log(isActive)
     }, [isActive])
 
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 400) {
+                setShowTopBtn(true);
+            } else {
+                setShowTopBtn(false);
+            }
+        });
+    }, []);
+    const goToTop = () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth",
+        });
+    };
     const showComponent = (e) => {
         setIsActive({
             id: e.target.id,
@@ -21,6 +37,8 @@ function ContextProvider({ children }) {
     const values = {
         mode, setMode,
         isOpen, setIsOpen,
+        showTopBtn, setShowTopBtn,
+        goToTop,
         isActive, setIsActive,
         showComponent
     }
