a064666b51f2c2570d9fb2011ca07931a6c0867c
iammmila <ibramila2002@gmail.com>
2023-02-20 18:46:37 -0800

    filter data is added

--- frontend/src/components/Main/Schedule/CardsSoon/CardsSoon.jsx
+++ frontend/src/components/Main/Schedule/CardsSoon/CardsSoon.jsx
@@ -12,12 +12,10 @@ import LoadingCard from './../LoadingCard/LoadingCard';
 import { MainContext } from '../../../../context/ContextProvider';
 
 function CardsSoon() {
-  const { soonFilms, setSoonFilms, loading, setLoading } = useContext(MainContext)
-
-  const URL = 'http://localhost:8080/films';
+  const { soonFilms, setSoonFilms, setLoading, filterTags, loading, FilmsURL } = useContext(MainContext)
 
   const getData = async () => {
-    await axios.get(URL).then((res) => setSoonFilms(res.data));
+    await axios.get(FilmsURL).then((res) => setSoonFilms(res.data));
     setLoading(false);
   }
 
@@ -32,23 +30,24 @@ function CardsSoon() {
         {
           loading ? (<><LoadingCard /> <LoadingCard /> <LoadingCard />
           </>) :
-            soonFilms?.map((data) => (
-              <li className="card_soon" key={data._id}>
-                {/* <img className='soon-poster' src={soon} alt="soon" /> */}
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-                <Link className="cardLink" to={`/film/${data._id}`}>
-                  <div className="card__background" style={{ backgroundImage: `url(${data.poster})` }}></div>
-                  <div className="card__content">
-                    <p className="card__category">{data.date}</p>
-                    <h3 className="card__heading">{data.name}</h3>
-                  </div>
-                </Link>
-              </li>
-            ))
-        }
+            soonFilms?.filter((film) => filterTags.length > 0 ? filterTags.every((filterTag) =>
+              film.formats.map((format) => format.name).includes(filterTag) || film.languages.map((language) => language.name).includes(filterTag)
+            ) : soonFilms)
+              .map((data) => (
+                <li className="card_soon" key={data._id}>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <Link className="cardLink" to={`/film/${data._id}`}>
+                    <div className="card__background" style={{ backgroundImage: `url(${data.poster})` }}></div>
+                    <div className="card__content">
+                      <p className="card__category">{data.date}</p>
+                      <h3 className="card__heading">{data.name}</h3>
+                    </div>
+                  </Link>
+                </li>
+              ))}
       </ul>
     </div>
   )
