e81a8bbc5a1738056b3a67245feecaab3e0b0141
Malahat Ibrahimova <ibramila2002@gmail.com>
2023-02-24 07:28:44 -0800

    folder structure is fixed

--- frontend/src/pages/Main/MainRoot.jsx
+++ frontend/src/pages/Main/MainRoot.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 
 //components
-import Footer from "../../components/Main/Footer/Footer";
-import Navbar from "../../components/Main/Navbar/Navbar";
+import Footer from "../../components/layout/Footer/Footer";
+import Navbar from "../../components/layout/Navbar/Navbar";
 import ScrollTopButton from './../../components/Main/ScrollTop/ScrollTopButton';
 
 function MainRoot() {
