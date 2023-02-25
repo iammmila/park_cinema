769c1cf51d69c7be28e61d8b334bc3d03e05ea06
Malahat Ibrahimova <ibramila2002@gmail.com>
2023-02-26 03:52:01 +0400

    changed`

--- frontend/src/components/Main/VIP/InfoHall/Information.jsx
+++ frontend/src/components/Main/VIP/InfoHall/Information.jsx
@@ -19,7 +19,7 @@ function Information() {
                             <p>VIP Lounge is the newest premium of the standard hall, equipped with a silver screen, with state-of-the-art film projection and audio equipment (three-way DOLBY Surround 5.1 sound), which allows you to watch movies in both 2D and 3D formats. Perfectly executed comfortable interior, special wall, and ceiling coverings absorb side tones, contributing to the clear sound of the film.</p>
                             <p>VIP Lounge has 23 luxurious leather armchairs of the Spanish firm “Figueras”, which have wide armrests, electric adjustment of the backrest inclination and footrest.</p>
                             <p>For more comfort, each chair is equipped with a control button, through which viewers can take a comfortable position and most importantly - you can order drinks and food to the hall, enjoying watching.</p>
-                            <p>VIP Lounge is for connoisseurs of good cinema and comfort, for those who prefer to gain only the best!</p>import SliderHall from './../SliderHall/SliderHall';
+                            <p>VIP Lounge is for connoisseurs of good cinema and comfort, for those who prefer to gain only the best!</p>
                             <p>We are waiting for you at the Park Cinema VIP Lounge, at Park Bulvar, on the 5th floor.</p>
                         </div>
                         {/* <div className='right_side'> */}
