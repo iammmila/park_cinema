c157d711aa0228db679f315b665a85c322f8eb76
Malahat Ibrahimova <ibramila2002@gmail.com>
2023-02-26 03:51:12 +0400

    changed

--- frontend/src/components/Main/VIP/InfoHall/Information.jsx
+++ frontend/src/components/Main/VIP/InfoHall/Information.jsx
@@ -14,7 +14,7 @@ function Information() {
                     <div style={{ width: "18%" }} className='line' ></div>
                     <h1>Azal Club VİP Hall</h1>
                     <div className='information-brief'>
-                        <div style={{ width: "50%", padding: "0 50px" }}>
+                        <div className='left_side'>
                             <span>Park Cinema VIP Lounge - for the real movie gourmets.</span>
                             <p>VIP Lounge is the newest premium of the standard hall, equipped with a silver screen, with state-of-the-art film projection and audio equipment (three-way DOLBY Surround 5.1 sound), which allows you to watch movies in both 2D and 3D formats. Perfectly executed comfortable interior, special wall, and ceiling coverings absorb side tones, contributing to the clear sound of the film.</p>
                             <p>VIP Lounge has 23 luxurious leather armchairs of the Spanish firm “Figueras”, which have wide armrests, electric adjustment of the backrest inclination and footrest.</p>
@@ -22,9 +22,12 @@ function Information() {
                             <p>VIP Lounge is for connoisseurs of good cinema and comfort, for those who prefer to gain only the best!</p>import SliderHall from './../SliderHall/SliderHall';
                             <p>We are waiting for you at the Park Cinema VIP Lounge, at Park Bulvar, on the 5th floor.</p>
                         </div>
-                        <div className='slider-hall-wrapper'>
-                            <SliderHall />
-                        </div>
+                        {/* <div className='right_side'> */}
+                            <div className='slider-hall-wrapper'>
+                                <SliderHall />
+                            </div>
+                        {/* </div> */}
+
                     </div>
                 </div>
             </div>
